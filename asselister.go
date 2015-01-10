ea32656257a901676661b3909daa2bc06ab221a1
Andrew Griffiths <mail@andrewgriffithsonline.com>
2015-01-10 01:04:42 +0000

    stub test

--- asselister.go
+++ asselister.go
@@ -2,32 +2,45 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io/ioutil"
 )
 
 // "fmt"
 
-type AssetLister struct {
+type Alister struct {
 	InputPath string
 	Data      map[string]interface{}
-	AssetList []string
+	Assets    []string
 }
 
-// func (a *AssetLister) readJSON(pathToJson string, map[string]interface{})  {
-func (a *AssetLister) LoadInput() {
-	file, err := ioutil.ReadFile(InputPath)
-	if err != nil {
+func (a Alister) loadInput() (io.Reader, error) {
+	return ioutil.ReadFile(InputPath)
+}
+
+func (a *Alister) marshallInput(in io.Reader) error {
+	buf := bytes.NewReader(bytes.Buffer)
+	buf.ReadFrom(in)
+	return json.Unmarshal(in, &a.Data)
+}
+
+// extract a list of assets from the JSON
+func (a *Alister) buildAssetList() error {
+	a.Assets{"hello", "world"}
+	return nil
+}
+
+func (a Alister) Run() error {
+
+	if file, err := a.loadInput(); err != nil {
 		return err
 	}
-	a.marshallInput(bytes.NewReader(file), a.Data)
+
+	if err = a.marshallInput(bytes.NewReader(file)); err != nil {
+		return err
+	}
+
+	if err = a.buildAssetList(); err != nil {
+		return err
+	}
 }
-
-func (a AssetLister) marshallInput(in io.Reader, d map[string]interface) {
-
-}
-
-// // extract a list of assets from the JSON
-// func (a *AssetLister) BuildAssetList(interface{}) []string {
-// 	return nil
-// 	// fmt.println
-// }
--- assetlister_test.go
+++ assetlister_test.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"os"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 )
 
-cwd := filepath.Abs(filepath.Dir(os.Args[0]))
+var cwd string = filepath.Abs(filepath.Dir(os.Args[0]))
 
-func readJSONTest(t *testing.T) {
+func TestRun(t *testing.T) {
 
-  a := AssetLister{filepath.Join(cwd, "fixtures", "cms.json")}
+	a := AssetLister{filepath.Join(cwd, "fixtures", "cms.json")}
 
-  a.LoadInput()
+	if err := a.Run(); err != nil {
+		t.Fatalf("%v", err)
+	}
+
+	if a.Assets[0] != "helsssl" {
+		t.Fatalf("%s", "should = heloo")
+	}
 
 }
