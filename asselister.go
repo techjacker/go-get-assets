80943c6133b7c7751fd914bb996e952c318a635d
Andrew Griffiths <mail@andrewgriffithsonline.com>
2015-01-12 20:21:01 +0000

    unmarshall json

--- asselister.go
+++ asselister.go
@@ -6,41 +6,44 @@ import (
 	"io/ioutil"
 )
 
-// "fmt"
-
 type Alister struct {
 	InputPath string
 	Data      map[string]interface{}
 	Assets    []string
 }
 
-func (a Alister) loadInput() (io.Reader, error) {
-	return ioutil.ReadFile(InputPath)
-}
-
-func (a *Alister) marshallInput(in io.Reader) error {
-	buf := bytes.NewReader(bytes.Buffer)
+func (a *Alister) readFile() ([]byte, error) {
+	// read file
+	file, err := ioutil.ReadFile(a.InputPath)
+	in := bytes.NewReader(file)
+	// create buffer of contents
+	buf := new(bytes.Buffer)
 	buf.ReadFrom(in)
-	return json.Unmarshal(in, &a.Data)
+	return buf.Bytes(), err
 }
 
 // extract a list of assets from the JSON
 func (a *Alister) buildAssetList() error {
-	a.Assets{"hello", "world"}
+	// a.Assets{"hello", "world"}
 	return nil
 }
 
-func (a Alister) Run() error {
+func (a *Alister) Run() error {
 
-	if file, err := a.loadInput(); err != nil {
+	contents, err := a.readFile()
+
+	if err != nil {
 		return err
 	}
 
-	if err = a.marshallInput(bytes.NewReader(file)); err != nil {
-		return err
-	}
+	err = json.Unmarshal(contents, &a.Data)
 
-	if err = a.buildAssetList(); err != nil {
-		return err
-	}
+	// if err = a.marshallInput(bytes.NewReader(file)); err != nil {
+	// 	return err
+	// }
+
+	// if err = a.buildAssetList(); err != nil {
+	// 	return err
+	// }
+	return err
 }
--- assetlister_test.go
+++ assetlister_test.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"io/ioutil"
-	"os"
-	"path/filepath"
+	// "flag"
+	// "path/filepath"
 	"testing"
 )
 
-var cwd string = filepath.Abs(filepath.Dir(os.Args[0]))
+// func init() {
+// 	flag.Parse()
+// }
+
+// var (
+// 	cwd = flag.String("cwd", "", "set cwd")
+// )
 
 func TestRun(t *testing.T) {
+	// t.Errorf("%v", &cwd)
 
-	a := AssetLister{filepath.Join(cwd, "fixtures", "cms.json")}
+	var a Alister
+	// a.InputPath = filepath.Join(cwd, "fixtures", "cms.json")
+	a.InputPath = "/home/andy/lib/modules/go/src/github.com/techjacker/go-get-assets/fixtures/cms.json"
 
 	if err := a.Run(); err != nil {
 		t.Fatalf("%v", err)
 	}
 
-	if a.Assets[0] != "helsssl" {
-		t.Fatalf("%s", "should = heloo")
-	}
+	t.Logf("%v", a.Data["nophotos"])
+	// if a.Assets[0] != "helsssl" {
+	// 	t.Fatalf("%s", "should = heloo")
+	// }
 
 }
