a0cc8fed00287d1a1a267ee983bdd8525ebf297f
Andrew Griffiths <mail@andrewgriffithsonline.com>
2015-01-30 11:34:09 +0000

    stub gdrive init

--- downloader.go
+++ downloader.go
@@ -1,21 +1,52 @@
 package main
 
 import (
-	// "code.google.com/p/google-api-go-client/drive/v2"
+	"code.google.com/p/google-api-go-client/drive/v2"
 	"fmt"
+	"net/http"
 )
 
+func New(Assets map[string]struct{}, outputDir string) (*Downloader, error) {
+	// client, err := auth.GetOauth2Client(config.ClientId, config.ClientSecret, tokenPath, promptUser)
+
+	client := http.Client{}
+
+	if err != nil {
+		return nil, err
+	}
+
+	drive, err := drive.New(client)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return a new authorized Drive client.
+	return &Downloader{
+		drive,
+		client,
+		Assets,
+		outputDir,
+	}, nil
+	// return &Drive{drive, client}, nil
+}
+
+type Drive struct {
+	*drive.Service
+	client *http.Client
+}
+
 type Downloader struct {
+	Drive
 	Assets    map[string]struct{}
 	outputDir string
 }
 
-func (d *Downloader) downloadAssets() error {
+func (d *Downloader) Multi() error {
 	fmt.Println("downloader")
 	return nil
 }
 
 // downloadAssets(assetList array, assetTypeLocation map)
-func (d *Downloader) downloadSingleAsset() error {
+func (d *Downloader) Single(g *gdrive.Drive) error {
 	return nil
 }
--- go-get-assets.go
+++ go-get-assets.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"code.google.com/p/google-api-go-client/drive/v2"
 	"fmt"
+
 	// "os"
 )
 
+// a.InputPath = filepath.Join(cwd, "fixtures", "cms.json")
+
 func main() {
-	var a Lister
-	// a.InputPath = filepath.Join(cwd, "fixtures", "cms.json")
-	a.InputPath = "/home/andy/lib/modules/go/src/github.com/techjacker/go-get-assets/fixtures/cms.json"
-	a.Needle = "http://gdrive.com"
-	a.Assets = make(map[string]struct{})
+	g, err := gdrive.New()
+	a := Lister{
+		"/home/andy/lib/modules/go/src/github.com/techjacker/go-get-assets/fixtures/cms.json",
+		"http://gdrive.com",
+		make(map[string]struct{}),
+	}
 
 	if err := a.Run(); err != nil {
 		fmt.Printf("%v", err)
 	}
+
 }
