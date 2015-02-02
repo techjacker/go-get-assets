8e51143eb71d8b67a9c4c65762c48adc54295d15
Andrew Griffiths <mail@andrewgriffithsonline.com>
2015-02-02 09:43:39 +0000

    fix tests

--- downloader.go
+++ downloader.go
@@ -1,53 +1,53 @@
 package main
 
-import (
-	// "code.google.com/p/google-api-go-client/drive/v2"
-	"fmt"
-	"github.com/google/google-api-go-client/drive/v2"
-	"net/http"
-)
+// import (
+// 	"code.google.com/p/google-api-go-client/drive/v2"
+// 	"fmt"
+// 	// "github.com/google/google-api-go-client/drive/v2"
+// 	"net/http"
+// )
 
-func New(Assets map[string]struct{}, outputDir string) (*Downloader, error) {
-	// client, err := auth.GetOauth2Client(config.ClientId, config.ClientSecret, tokenPath, promptUser)
+// func NewDownloader(Assets map[string]struct{}, outputDir string) (*Downloader, error) {
+// 	// client, err := auth.GetOauth2Client(config.ClientId, config.ClientSecret, tokenPath, promptUser)
 
-	client := http.Client{}
+// 	client := http.Client{}
 
-	if err != nil {
-		return nil, err
-	}
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	drive, err := drive.New(client)
-	if err != nil {
-		return nil, err
-	}
+// 	drive, err := drive.New(client)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	// Return a new authorized Drive client.
-	return &Downloader{
-		drive,
-		client,
-		Assets,
-		outputDir,
-	}, nil
-	// return &Drive{drive, client}, nil
-}
+// 	// Return a new authorized Drive client.
+// 	return &Downloader{
+// 		drive,
+// 		client,
+// 		Assets,
+// 		outputDir,
+// 	}, nil
+// 	// return &Drive{drive, client}, nil
+// }
 
-type Drive struct {
-	*drive.Service
-	client *http.Client
-}
+// type Drive struct {
+// 	*drive.Service
+// 	client *http.Client
+// }
 
-type Downloader struct {
-	Drive
-	Assets    map[string]struct{}
-	outputDir string
-}
+// type Downloader struct {
+// 	Drive
+// 	Assets    map[string]struct{}
+// 	outputDir string
+// }
 
-func (d *Downloader) Multi() error {
-	fmt.Println("downloader")
-	return nil
-}
+// func (d *Downloader) Multi() error {
+// 	fmt.Println("downloader")
+// 	return nil
+// }
 
-// downloadAssets(assetList array, assetTypeLocation map)
-func (d *Downloader) Single(g *gdrive.Drive) error {
-	return nil
-}
+// // downloadAssets(assetList array, assetTypeLocation map)
+// func (d *Downloader) Single(g *gdrive.Drive) error {
+// 	return nil
+// }
--- go-get-assets.go
+++ go-get-assets.go
@@ -8,12 +8,16 @@ import (
 // a.InputPath = filepath.Join(cwd, "fixtures", "cms.json")
 
 func main() {
-	g, err := gdrive.New()
-	a := Lister{
-		"/home/andy/lib/modules/go/src/github.com/techjacker/go-get-assets/fixtures/cms.json",
-		"http://gdrive.com",
-		make(map[string]struct{}),
-	}
+	// // g, err := gdrive.New()
+	// a := Lister{
+	// 	"http://gdrive.com",
+	// 	"/home/andy/lib/modules/go/src/github.com/techjacker/go-get-assets/fixtures/cms.json",
+	// 	// struct{}{},
+	// 	// make(map[string]struct{}),
+	// }
+	var a Lister
+	a.Needle = "http://gdrive.com"
+	a.InputPath = "/home/andy/lib/modules/go/src/github.com/techjacker/go-get-assets/fixtures/cms.json"
 
 	if err := a.Run(); err != nil {
 		fmt.Printf("%v", err)
