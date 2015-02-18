f3493747312ee81299412655be173089acd6514b
Andrew Griffiths <mail@andrewgriffithsonline.com>
2015-02-18 09:51:35 +0000

    remove so question + gdrive api lib + stub download with http.client method

--- downloader.go
+++ downloader.go
@@ -1,67 +1,22 @@
 package main
 
 import (
-	"code.google.com/p/google-api-go-client/drive/v2"
-	// "github.com/google/google-api-go-client/drive/v2"
+	"fmt"
 	"net/http"
 )
-
-func ExtractId(url string) string {
-	return ""
-}
-
-func NewDownloader(assets map[string]struct{}, outputDir string) (*Downloader, error) {
-
-	// client := http.Client{}
-	// drive, err := drive.New(&client)
-	client := new(http.Client)
-	drive, err := drive.New(client)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &Downloader{
-		outputDir,
-		assets,
-		map[string]DFile{},
-		drive.Files,
-		// &Drive{
-		// 	drive.Files,
-		// 	&client,
-		// },
-	}, nil
-}
 
 type Downloader struct {
 	OutputDir string
 	Assets    map[string]struct{}
-	Metadata  map[string]DFile
-	Service   DFilesService
-	// Drive     *Drive
-}
-
-func (d *Downloader) GetInfoAll() error {
-	// map["http://gdrive.com/traffic.jpg":{}]
-	var err error
-	for i := range d.Assets {
-		d.Metadata[i], err = d.GetInfo(i, d.Service)
-	}
-	return err
-}
-
-// /home/andy/go/src/code.google.com/p/google-api-go-client/drive/v2/drive-gen.go
-// line 3536
-func (d *Downloader) GetInfo(id string, dService DFilesService) (DFile, error) {
-	f, err := dService.Get(id).Do()
-	return DFile{
-		f.DownloadUrl,
-		f.FileExtension,
-		f.Title,
-	}, err
 }
 
 func (d *Downloader) Download(url string) error {
+
+	c := new(http.Client)
+
+	fmt.Print(c)
+	fmt.Print(url)
+
 	// if f.DownloadUrl == "" {
 	// 	return DFile{}, fmt.Errorf("An error occurred: File is not downloadable")
 	// }
--- downloader_test.go
+++ downloader_test.go
@@ -5,19 +5,20 @@ import (
 	"testing"
 )
 
-func TestNewDownloader(t *testing.T) {
-	assets := map[string]struct{}{
-		"http://gdrive.com/traffic.jpg": {},
-	}
-	var out string = os.TempDir() + "/go-get-assets"
+func TestDownload(t *testing.T) {
 
-	d, err := NewDownloader(assets, out)
-
-	if err != nil {
-		t.Fatal("error initialising drive", err)
+	d := Downloader{
+		os.TempDir() + "/go-get-assets",
+		map[string]struct{}{
+			"http://gdrive.com/traffic.jpg": {},
+		},
 	}
 
-	// t.Logf("%q", d.Drive.DFilesService)
-	// t.Logf("%q", d.Drive)
-	t.Log(d)
+	d.Download("http://google.com")
+
+	// if err != nil {
+	// 	t.Fatal("error initialising drive", err)
+	// }
+
+	// t.Log(d)
 }
